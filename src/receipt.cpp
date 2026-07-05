7f97f2ef3eb7d8e950e6b22c8c5cb2bebceb8c58
andrewchencc <achen026@student.sdccd.edu>
2026-07-05 19:58:38 +0000

    added suggestion to receipt.cpp

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -7,17 +7,17 @@ double calculateSubtotal(double item1, double item2, double item3) {
 
 // TODO: Return the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
-    return subtotal*TAX_RATE;
+    return subtotal * TAX_RATE; //achen: added spaces so it looks better
 }
 
 // TODO: Return the tip amount.
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
-    return subtotal*tipPercent/100.0;
+    return subtotal * (tipPercent / 100.0); //achen: added spaces and paratheses to make it more clear
 }
 
 // TODO: Return the final total.
 double calculateTotal(double subtotal, double tax, double tip) {
-    return subtotal+tax+tip;
+    return subtotal + tax + tip; //achen: added spaces again
 }
